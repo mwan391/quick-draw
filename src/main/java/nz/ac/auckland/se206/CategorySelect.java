7e8229abe7beec0f85c277fd951606ca29a00e2b
jrut644 <jrut644@aucklanduni.ac.nz>
2022-09-04 14:52:51 +1200

    feat - import janna's code to use as base
    Copy pasted entire src folder from Janna's alpha code on top of the beta code.

---
+++ src/main/java/nz/ac/auckland/se206/CategorySelect.java
@@ -0,0 +1,74 @@
+package nz.ac.auckland.se206;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class CategorySelect {
+
+  public enum Difficulty {
+    EASY,
+    MEDIUM,
+    HARD
+  }
+
+  private static HashMap<Difficulty, ArrayList<String>> categories = new HashMap<>();
+  private static String category = null;
+
+  /*
+   * This method is invoked when the application starts. It loads the given csv file into a hashmap
+   * corresponding to the difficulty.
+   *
+   * @param csvName the name of the csv file in the resource folder.
+   *
+   * @throws IOException If "src/main/resources/csvName.csv" is not found.
+   *
+   */
+  public static void setCategories(String csvName) throws IOException {
+
+    HashMap<String, ArrayList<String>> tempCategories = new HashMap<>();
+    tempCategories.put("E", new ArrayList<>());
+    tempCategories.put("M", new ArrayList<>());
+    tempCategories.put("H", new ArrayList<>());
+
+    URL csvFile = CategorySelect.class.getResource("/" + csvName + ".csv");
+
+    // code adapted from https://cowtowncoder.medium.com/reading-csv-with-jackson-c4e74a15ddc1
+    final CsvMapper mapper = new CsvMapper();
+    // read csv file lines into an iterator class
+    MappingIterator<List<String>> csvValues =
+        mapper
+            .readerForListOf(String.class)
+            .with(CsvParser.Feature.WRAP_AS_ARRAY)
+            .readValues(csvFile);
+    // iterate through every line in the csv and assign the values to the corresponding array
+    while (csvValues.hasNextValue()) {
+      List<String> row = csvValues.nextValue();
+      tempCategories.get(row.get(1)).add(row.get(0));
+    }
+
+    // move the arrays into the proper hashmap with the enum keys
+    categories.put(Difficulty.EASY, tempCategories.get("E"));
+    categories.put(Difficulty.MEDIUM, tempCategories.get("M"));
+    categories.put(Difficulty.HARD, tempCategories.get("H"));
+  }
+
+  public static String generateCategory(Difficulty wordDifficulty) {
+
+    ArrayList<String> words = categories.get(wordDifficulty);
+
+    // get random word in array
+    int randomNum = (int) Math.floor(Math.random() * words.size());
+    category = words.get(randomNum);
+    return category;
+  }
+
+  public static String getCategory() {
+    return category;
+  }
+}
---
+++ src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
@@ -0,0 +1,38 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
+import nz.ac.auckland.se206.speech.TextToSpeech;
+
+public class MenuController implements Controller {
+
+  @FXML private Button btnStart;
+
+  @FXML
+  private void onStartGame(ActionEvent event) {
+    // change the scene
+    Scene scene = ((Button) event.getSource()).getScene();
+    scene.setRoot(SceneManager.getUiRoot(AppUi.CATEGORY_SELECT));
+
+    // run the text to speech on a background thread to avoid lags
+    TextToSpeech textToSpeech = new TextToSpeech();
+    Task<Void> backgroundTask =
+        new Task<Void>() {
+
+          @Override
+          protected Void call() throws Exception {
+            textToSpeech.speak("Pick a difficulty.");
+            return null;
+          }
+        };
+
+    // start the thread
+    Thread backgroundThread = new Thread(backgroundTask);
+    backgroundThread.start();
+  }
+}
