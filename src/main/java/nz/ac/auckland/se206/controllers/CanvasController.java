6d9848ff75acc059845d43e915e687e9c90ffafe
J-Rutor <97650514+J-Rutor@users.noreply.github.com>
2022-09-16 18:24:46 +1200

    docu - remove automatically generated todo comment

--- src/main/java/nz/ac/auckland/se206/controllers/CanvasController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CanvasController.java
@@ -366,7 +366,6 @@ public class CanvasController implements Controller {
             try {
               Thread.sleep(1000);
             } catch (InterruptedException e1) {
-              // TODO Auto-generated catch block
               e1.printStackTrace();
             }
 
