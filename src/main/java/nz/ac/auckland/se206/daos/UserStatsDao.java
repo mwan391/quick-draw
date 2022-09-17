7426f988ad12d346d4fe08ecc1733d270f3697aa
Michelle <mwan391@aucklanduni.ac.nz>
2022-09-17 16:21:19 +1200

    feat - get num of games played by given user

---
+++ src/main/java/nz/ac/auckland/se206/daos/UserStatsDao.java
@@ -0,0 +1,21 @@
+package nz.ac.auckland.se206.daos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import nz.ac.auckland.se206.util.SqliteConnection;
+
+public class UserStatsDao {
+
+  public int countGames(int userId) throws SQLException {
+    Connection connection = SqliteConnection.openConnection();
+    String query = "SELECT COUNT(*) AS count FROM games WHERE user_id=?";
+    PreparedStatement ps = connection.prepareStatement(query);
+    ps.setInt(1, userId);
+    ResultSet rs = ps.executeQuery();
+    int count = rs.next() ? rs.getInt("count") : 0;
+    SqliteConnection.closeConnection(connection);
+    return count;
+  }
+}
