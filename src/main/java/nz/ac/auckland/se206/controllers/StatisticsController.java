fe6d95b99c384ebc0339102a3b686779e291ebbd
jrut644 <jrut644@aucklanduni.ac.nz>
2022-09-22 23:16:08 +1200

    feat - return to category select from statistics page

--- src/main/java/nz/ac/auckland/se206/controllers/StatisticsController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StatisticsController.java
@@ -1,9 +1,14 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class StatisticsController implements Controller {
 
@@ -16,5 +21,9 @@ public class StatisticsController implements Controller {
   @FXML private Button btnBackToMenu;
 
   @FXML
-  private void onBackToMenu() {}
+  private void onBackToMenu(ActionEvent event) {
+    Scene scene = ((Button) event.getSource()).getScene();
+    Parent logInRoot = SceneManager.getUiRoot(AppUi.CATEGORY_SELECT);
+    scene.setRoot(logInRoot);
+  }
 }
