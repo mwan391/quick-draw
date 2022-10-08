db6aa22d582e7aa4301700e3bfde6d37089ac39b
Michelle <mwan391@aucklanduni.ac.nz>
2022-10-08 13:51:16 +1300

    feat - retrives all user's played games as a map where the key is its difficulty (enum) and its values are the list of games with that difficulty

--- src/main/java/nz/ac/auckland/se206/daos/UserStatsDao.java
+++ src/main/java/nz/ac/auckland/se206/daos/UserStatsDao.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import nz.ac.auckland.se206.CategorySelect.Difficulty;
 import nz.ac.auckland.se206.models.GameModel;
 import nz.ac.auckland.se206.util.Logger;
 import nz.ac.auckland.se206.util.SqliteConnection;
@@ -116,14 +119,56 @@ public class UserStatsDao {
     return games;
   }
 
-  private GameModel getGame(ResultSet rs) throws SQLException {
+  private GameModel getGame(ResultSet rs) {
     // helper to convert to game instance
-    return new GameModel(
-        rs.getInt("id"),
-        rs.getInt("user_id"),
-        rs.getString("difficulty"),
-        rs.getString("word"),
-        rs.getBoolean("won"),
-        rs.getInt("time"));
+    GameModel game = null;
+    try {
+      // translate each column to its game field
+      game =
+          new GameModel(
+              rs.getInt("id"),
+              rs.getInt("user_id"),
+              rs.getString("difficulty"),
+              rs.getString("word"),
+              rs.getBoolean("won"),
+              rs.getInt("time"));
+    } catch (SQLException e) {
+      Logger.printSqlError(e);
+    }
+    return game;
+  }
+
+  private Map<Difficulty, List<GameModel>> rsToMap(
+      ResultSet rs, Map<Difficulty, List<GameModel>> map) {
+    GameModel game = getGame(rs);
+    // convert difficulty (string) to difficulty (enum)
+    Difficulty gDifficulty = Difficulty.valueOf(game.getDifficulty());
+    // initiate list for each key
+    map.computeIfAbsent(gDifficulty, d -> new ArrayList<>());
+    map.get(gDifficulty).add(game);
+    return map;
+  }
+
+  public Map<Difficulty, List<GameModel>> getMapHistory(int userId) {
+    Map<Difficulty, List<GameModel>> map =
+        new EnumMap<Difficulty, List<GameModel>>(Difficulty.class);
+    Connection connection = SqliteConnection.openConnection();
+    try {
+      // query for all games player by user
+      String query = "SELECT * FROM games WHERE user_id=? ORDER BY id";
+      PreparedStatement ps = connection.prepareStatement(query);
+      // input specific user
+      ps.setInt(1, userId);
+      ResultSet rs = ps.executeQuery();
+      // map each difficulty to its games e.g. EASY -> [game1, game2, game3]
+      while (rs.next()) {
+        map = rsToMap(rs, map);
+      }
+    } catch (SQLException e) {
+      Logger.printSqlError(e);
+    } finally {
+      SqliteConnection.closeConnection(connection);
+    }
+    return map;
   }
 }
