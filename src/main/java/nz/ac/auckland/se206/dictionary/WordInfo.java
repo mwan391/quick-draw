a0950a6b0f16c46de080783e78337b72f725867a
Michelle <mwan391@aucklanduni.ac.nz>
2022-10-12 11:32:40 +1300

    refactor - move WordInfo class from models to dictionary directory

--- src/main/java/nz/ac/auckland/se206/dictionary/WordInfo.java
+++ src/main/java/nz/ac/auckland/se206/dictionary/WordInfo.java
@@ -1,15 +1,14 @@
-package nz.ac.auckland.se206.models;
+package nz.ac.auckland.se206.dictionary;
 
 import nz.ac.auckland.se206.CategorySelect.Difficulty;
-import nz.ac.auckland.se206.util.Meaning;
 
-public class WordInfoModel {
+public class WordInfo {
 
   private String word;
   private Meaning meaning;
   private Difficulty difficulty;
 
-  public WordInfoModel(String word, Meaning meaning, Difficulty difficulty) {
+  public WordInfo(String word, Meaning meaning, Difficulty difficulty) {
     this.word = word;
     this.meaning = meaning;
     this.difficulty = difficulty;
