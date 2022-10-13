703a872a046cd64e0ac3b0566670ae54d49014c2
Michelle <mwan391@aucklanduni.ac.nz>
2022-10-13 18:47:02 +1300

    docs - add comments to model constructor

--- src/main/java/nz/ac/auckland/se206/models/GameModel.java
+++ src/main/java/nz/ac/auckland/se206/models/GameModel.java
@@ -9,6 +9,16 @@ public class GameModel {
   private boolean won;
   private int time;
 
+  /**
+   * This models what how a 'game' is structured in SQLite, each field is a column title
+   *
+   * @param id unique id of game session
+   * @param userId user that played this game
+   * @param difficulty difficulty of words of this game
+   * @param word that was played in this game
+   * @param won boolean of whether this game was won or lost
+   * @param time time took to win the game, or by default 60 seconds if game was lost
+   */
   public GameModel(int id, String userId, String difficulty, String word, boolean won, int time) {
     this.id = id;
     this.userId = userId;
