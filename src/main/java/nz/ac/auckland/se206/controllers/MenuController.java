da17cc61dd87ae67954800a2b62011d348f02a3f
Michelle <mwan391@aucklanduni.ac.nz>
2022-10-16 20:44:15 +1300

    feat - add pulse to quick draw icon at start menu

--- src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
@@ -1,10 +1,15 @@
 package nz.ac.auckland.se206.controllers;
 
+import animatefx.animation.Pulse;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 import nz.ac.auckland.se206.SoundManager;
@@ -14,6 +19,16 @@ import nz.ac.auckland.se206.speech.TextToSpeech;
 public class MenuController implements Controller {
 
   @FXML private Button btnStart;
+
+  @FXML private ImageView image;
+
+  /** This method sets up the pulse animation for the Quick Draw pencil logo */
+  public void initialize() {
+    // Animate the Quick Draw pencil icon
+    Timeline time = new Timeline(new KeyFrame(Duration.millis(2000), e -> new Pulse(image).play()));
+    time.setCycleCount(Timeline.INDEFINITE);
+    time.play();
+  }
 
   /**
    * This method takes the user to the sign in screen and plays appropriate sound effect + tts
