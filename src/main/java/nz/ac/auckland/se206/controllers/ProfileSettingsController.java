21ba3fcbfc383aec1afa9bda5ff6abc38b24df02
jrut644 <jrut644@aucklanduni.ac.nz>
2022-10-16 16:00:10 +1300

    feat - save new pfp to database on return to menu

--- src/main/java/nz/ac/auckland/se206/controllers/ProfileSettingsController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ProfileSettingsController.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.util.Optional;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -7,12 +8,18 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 import nz.ac.auckland.se206.SoundManager;
+import nz.ac.auckland.se206.daos.UserDaoJson;
 import nz.ac.auckland.se206.models.UserModel;
 
 public class ProfileSettingsController implements Controller {
@@ -52,6 +59,8 @@ public class ProfileSettingsController implements Controller {
     Parent categoryRoot = SceneManager.getUiRoot(AppUi.CATEGORY_SELECT);
 
     // save image to database
+    UserDaoJson userDao = new UserDaoJson();
+    userDao.updateAvatar(activeUser, picChooser.getValue());
 
     // change scene
     SoundManager.playSound();
@@ -68,8 +77,40 @@ public class ProfileSettingsController implements Controller {
     // Changing the preview to the new selection
     picPreview.setImage(preview);
     SoundManager.playSound();
+  }
 
-    // save image to model
-    activeUser.setIcon(picChooser.getValue());
+  /**
+   * This method will show a warning pop up. clicking delete will delete everything and take the
+   * user back to the first screen, while clicking cancel will do nothing.
+   */
+  @FXML
+  private void onDeleteAccount() {
+
+    // create popup
+    Dialog<Void> badgePopup = new Dialog<>();
+
+    // write main dialog
+    String str =
+        "WARNING!\nYou will lose all progress, and you cannot undo this.\n\nAre you sure you want to delete everything?";
+    badgePopup.setContentText(str);
+    // add buttons
+    ButtonType btnDelete = new ButtonType("Delete Account", ButtonData.OK_DONE);
+    badgePopup.getDialogPane().getButtonTypes().add(btnDelete);
+    ButtonType btnCancel = new ButtonType("Go Back", ButtonData.CANCEL_CLOSE);
+    badgePopup.getDialogPane().getButtonTypes().add(btnCancel);
+
+    // change the top title
+    badgePopup.setTitle("WARNING!");
+    // set size of dialog and buttons
+    DialogPane popupPane = badgePopup.getDialogPane();
+    popupPane.setPrefSize(550, 200);
+    popupPane.getButtonTypes().stream()
+        .map(popupPane::lookupButton)
+        .forEach(btn -> ButtonBar.setButtonUniformSize(btn, false));
+    // set css formatting for pane and buttons
+    popupPane.getStylesheets().add("/css/style.css");
+
+    // show badge
+    Optional<Void> result = badgePopup.showAndWait();
   }
 }
